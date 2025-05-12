d0e8631244fccf4c9c33d1527165704c5f75273c
SunnyDevZH <yannick.vaterlaus@windowslive.com>
2025-05-12 20:42:48 +0200

    refactor Login and Header components; update styles for consistency and improved UI; add mail icon to input fields

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import './styles/global.css';
 
+
 function App() {
   return (
     <Router>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import logo from '../assets/images/logo.svg'; // Importiere das Logo
-import styles from '../styles/components/Header.module.css'; // Importiere das CSS-Modul
+import { useLocation } from 'react-router-dom';
+import logo from '../assets/images/logo.svg';
+import styles from '../styles/components/Header.module.css';
 
 function Header() {
+    /* Login Button ausblenden in LoginPage */
+  const location = useLocation(); 
+  const isLoginPage = location.pathname === '/login';
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <img src={logo} alt="Videoflix Logo" className={styles.logoImage} />
       </div>
       <nav className={styles.nav}>
-        <button className={styles.loginButton}>Log in</button>
+        {!isLoginPage && (<button className={styles.loginButton}>Log in</button>)}
       </nav>
     </header>
   );
