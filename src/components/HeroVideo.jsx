29c79784d552107c5cedd613d6a61eb9338697ea
SunnyDevZH <yannick.vaterlaus@windowslive.com>
2025-05-13 21:20:49 +0200

    refactor VideoOffer and HeroVideo components; update styles for improved layout and organization; replace video with image in HeroVideo; add new video thumbnails for categories

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import styles from '../styles/components/HeroVideo.module.css';
+import bild from '../assets/images/Frame3.jpg';
 
 function HeroVideo() {
     return (
         <div className={styles.heroContainer}>
-            <video
+            {/* Bild anstelle des Videos */}
+            <img
                 className={styles.heroVideo}
-                autoPlay
-                muted
-                loop
-                playsInline
-                src="/videos/breakout-trailer.mp4"
-                poster="/images/breakout-poster.jpg"
+                src={bild}
+                alt="Breakout Poster"
             />
             <div className={styles.overlay}>
                 <h1>Breakout</h1>
--- src/pages/VideoOffer.jsx
+++ src/pages/VideoOffer.jsx
@@ -2,52 +2,53 @@ import React from 'react';
 import HeroVideo from '../components/HeroVideo';
 import CategoryRow from '../components/CategoryRow';
 import styles from '../styles/pages/VideoOffer.module.css';
-import logo from '../assets/icons/symbol.svg'; // Importiere das Logo
-import logoutIcon from '../assets/icons/logout.svg'; // Importiere das Logout-Icon
+import logo from '../assets/icons/symbol.svg';
+import logoutIcon from '../assets/icons/logout.svg';
+import Frame1 from '../assets/images/Frame1.jpg';
+import Frame2 from '../assets/images/Frame2.jpg';
+import Frame3 from '../assets/images/Frame3.jpg';
+import Frame4 from '../assets/images/Frame4.jpg';
+import Frame5 from '../assets/images/Frame5.jpg';
+import Frame6 from '../assets/images/Frame6.jpg';
+import Frame7 from '../assets/images/Frame7.jpg';
+
+export const newVideos = [
+    { thumbnail: Frame1, title: 'Frame 1' },
+    { thumbnail: Frame2, title: 'Frame 2' },
+    { thumbnail: Frame3, title: 'Frame 3' },
+    { thumbnail: Frame4, title: 'Frame 4' },
+    { thumbnail: Frame5, title: 'Frame 5' },
+    { thumbnail: Frame6, title: 'Frame 6' },
+    { thumbnail: Frame7, title: 'Frame 7' },
+];
+
+export const documentaries = [...newVideos];
+export const dramas = [...newVideos];
+export const romances = [...newVideos];
 
 function VideoOfferPage() {
-  const newVideos = [
-    { thumbnail: '/images/video1.jpg', title: 'Video 1' },
-    { thumbnail: '/images/video2.jpg', title: 'Video 2' },
-    { thumbnail: '/images/video3.jpg', title: 'Video 3' },
-  ];
-  const documentaries = [
-    { thumbnail: '/images/video1.jpg', title: 'Video 1' },
-    { thumbnail: '/images/video2.jpg', title: 'Video 2' },
-    { thumbnail: '/images/video3.jpg', title: 'Video 3' },
-  ];
-  const dramas = [
-    { thumbnail: '/images/video1.jpg', title: 'Video 1' },
-    { thumbnail: '/images/video2.jpg', title: 'Video 2' },
-    { thumbnail: '/images/video3.jpg', title: 'Video 3' },
-  ];
-  const romances = [
-    { thumbnail: '/images/video1.jpg', title: 'Video 1' },
-    { thumbnail: '/images/video2.jpg', title: 'Video 2' },
-    { thumbnail: '/images/video3.jpg', title: 'Video 3' },
-  ];
 
-  const handleLogout = () => {
-    console.log('User logged out');
-    // Hier kannst du die Logout-Logik implementieren
-  };
+    const handleLogout = () => {
+        console.log('User logged out');
 
-  return (
-    <div className={styles.videoOfferPage}>
-      <header className={styles.header}>
-        <img src={logo} alt="Logo" className={styles.logo} />
-        <button className={styles.logoutButton} onClick={handleLogout}>
-          <img src={logoutIcon} alt="Logout" className={styles.logoutIcon} />
-          Log Out
-        </button>
-      </header>
-      <HeroVideo />
-      <CategoryRow title="New on Videoflix" videos={newVideos} />
-      <CategoryRow title="Documentary" videos={documentaries} />
-      <CategoryRow title="Drama" videos={dramas} />
-      <CategoryRow title="Romance" videos={romances} />
-    </div>
-  );
+    };
+
+    return (
+        <div className={styles.videoOfferPage}>
+            <header className={styles.header}>
+                <img src={logo} alt="Logo" className={styles.logo} />
+                <button className={styles.logoutButton} onClick={handleLogout}>
+                    <img src={logoutIcon} alt="Logout" className={styles.logoutIcon} />
+                    Log Out
+                </button>
+            </header>
+            <HeroVideo />
+            <CategoryRow title="New on Videoflix" videos={newVideos} />
+            <CategoryRow title="Documentary" videos={documentaries} />
+            <CategoryRow title="Drama" videos={dramas} />
+            <CategoryRow title="Romance" videos={romances} />
+        </div>
+    );
 }
 
 export default VideoOfferPage;
